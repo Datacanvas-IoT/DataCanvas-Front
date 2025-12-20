ebbb824d83b41ab6f0d68420651275efcc294585
kavindarlm <kavindarlm.21@uom.lk>
2025-12-21 01:22:12 +0530

    refactor: standardize quotes in AccessTokenCard component

--- src/components/cards/AccessTokenCard.js
+++ src/components/cards/AccessTokenCard.js
@@ -1,5 +1,5 @@
-import React from "react";
-import { FaTrash, FaPencilAlt } from "react-icons/fa";
+import React from 'react';
+import { FaTrash, FaPencilAlt } from 'react-icons/fa';
 
 const AccessTokenCard = ({ token, onEdit, onDelete }) => {
   return (
